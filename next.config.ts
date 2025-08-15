a0652dcda97a7e82fa95fe18ae682fd538f40a30
kevinsultana <kevinpdg.ks@gmail.com>
2025-08-16 04:46:06 +0700

    feat: adding metadata to homepage

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "static01.nyt.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
